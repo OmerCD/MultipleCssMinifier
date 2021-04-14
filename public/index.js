d3d057dee55cf918d49a2c6db41999d2f2561dfa
ksnet\09371238 <omercan.danacioglu@ext.kocsistem.com.tr>
2021-04-14 23:05:28 +0300

    Added download section

--- public/index.js
+++ public/index.js
@@ -53,12 +53,7 @@ function sendRequest(keys) {
         var element = document.createElement('a');
         element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
         element.setAttribute('download', filename);
-
-        element.style.display = 'none';
-        document.body.appendChild(element);
-
-        element.click();
-
-        document.body.removeChild(element);
+        element.text = filename;
+        document.getElementById("downloader").appendChild(element);
     }
 }
